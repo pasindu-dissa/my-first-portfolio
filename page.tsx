3fdb59198404686f2737df7e229a32ab4efcdd71
Pasindu Dissanayake <pasindudissanayake035@gmail.com>
2025-06-05 00:08:35 +0530

    mobile responsive features added (home page)
    created to responsive in mobile phone screens to home page only

--- page.tsx
+++ page.tsx
@@ -45,13 +45,17 @@ export default function Home() {
 
       {/*HERO SECTION*/}
       <section className="">
-        <div className=" flex justify-between mobile:max-md:block tab:max-laptop:block">
+        <div className=" flex justify-between 
+                        mobile:max-mobileLg:bg-[url(./assets/me.png)] mobile:max-mobileLg:bg-cover mobile:max-mobileLg:bg-position-[top_right_0.5rem] mobile:max-mobileLg:bg-no-repeat
+                        mobile:max-md:block   
+                        tab:max-laptop:block">
           {/*IMAGE*/}
           <Image
             src={profileImg}
             alt="profile-image"
-            className=" mobile:max-tab:w-100 mobile:max-tab:h-101 mobile:max-tab:relative mobile:max-tab:right-35
-                        tab:max-laptop:w-110 tab:max-laptop:h-111 tab:max-laptop:relative tab:max-laptop:right-35
+            className=" mobile:max-mobileLg:hidden
+                        mobileLg:max-tab:w-100 mobileLg:max-tab:h-101 mobileLg:max-tab:relative mobileLg:max-tab:right-35
+                        tab:max-laptop:w-110 tab:max-laptop:h-111 tab:max-laptop:relative tab:max-laptop:right-40
                         medium:max-lg:w-110 medium:max-lg:h-111 medium:max-lg:relative medium:max-lg:right-10
                         lg:max-laptop:w-110 lg:max-laptop:h-111 lg:max-laptop:relative lg:max-laptop:right-0
                         laptop:w-130 laptop:h-131 
@@ -60,11 +64,14 @@ export default function Home() {
           {/*ABOUT-ME*/}
           <div className=" flex flex-col items-end
                            gap-5
-                           laptop:gap-8 
-                           mobile:max-tab:absolute mobile:max-tab:top-25 mobile:max-tab:right-5 mobile:max-tab:w-3/5
+                           laptop:gap-8
+                           mobile:max-mobileLg:backdrop-brightness-50 
+                           mobile:max-mobileLg:pl-10
+                           mobileLg:max-tab:absolute mobileLg:max-tab:top-25 mobileLg:max-tab:right-5 mobileLg:max-tab:w-3/5
                            tab:max-laptop:absolute tab:max-laptop:top-25 tab:max-laptop:right-15 tab:max-laptop:w-3/5">
             <div
-              className=" pt-3 text-right font-antonio font-light 
+              className=" pt-3 text-right font-antonio font-light
+                          mobile:max-mobileLg:text-white 
                           mobile:text-[60px] mobile:leading-13.5 mobile:pb-2
                           tab:text-[70px] tab:leading-17
                           laptop:text-[85px] laptop:pt-8 laptop:pb-2 laptop:leading-20
@@ -87,7 +94,8 @@ export default function Home() {
             </div>
             <div>
               <span
-                className=" font-poppins font-medium text-[19px] p-1 bg-white text-black border-2 border-white
+                className=" font-poppins font-medium text-[19px] p-1 mobile:max-mobileLg:py-0.25 
+              bg-white text-black border-2 border-white
               cursor-pointer transition delay-50 duration-200 ease-in-out
               hover:bg-black hover:text-white hover:border-2 hover:te"
               >
